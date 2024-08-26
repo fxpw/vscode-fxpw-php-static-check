83d7980de3c1dab69d4d43a24ca0b7c58ba7240b
fxpw <fxpw@outlook.com>
2024-08-26 10:58:29 +0300

    test

--- ts/initExtension.ts
+++ ts/initExtension.ts
@@ -2,13 +2,11 @@ import * as vscode from 'vscode';
 import {PhpBladeСheck} from './PhpBladeСheck';
 export async function activate(context: vscode.ExtensionContext) {
 	try {
-		
 		await PhpBladeСheck.Init(context);
 		console.log("init vscode-fxpw-php-static-check v1.0.3");
 	} catch (error) {
 		console.error(error);
 	}
-
 }
 
 // This method is called when your extension is deactivated
